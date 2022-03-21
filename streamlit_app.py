3f3870a9ff693d624f6d14a75d55616b5bce0e7b
Isdanbull <101053821+Isdanbull@users.noreply.github.com>
2022-03-21 16:58:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+@st.experimental_singleton
 df = pd.read_csv("https://oracleselixir-downloadable-match-data.s3-us-west-2.amazonaws.com/2021_LoL_esports_match_data_from_OraclesElixir_20220321.csv")
 
 st.title("I'm a test app")
@@ -28,3 +29,9 @@ if button1:
 fig, ax = plt.subplots()
 ax = sns.violinplot(data=df[(df.league.isin(list1)) & (df.position.isin(list2))], y = "position", x = "kills")
 st.pyplot(fig)
+
+with st.expander("player stats"):
+  player = st.selectbox("player", df[(df.league.isin(list1)) & (df.position.isin(list2))].playername.unique())
+  p_df = df[df.playername == player]
+  st.write(f"Most played champion: {p_df.champion.mode()})
+  
